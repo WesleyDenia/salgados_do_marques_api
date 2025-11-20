9f24529c3466cb1750ac41f79aedbc1ecc06b70b
Wesley Silva <wesleydenia@gmail.com>
2025-11-20 14:34:13 +0000

    ajuste no comando docker

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -10,15 +10,15 @@ git checkout main
 git pull --ff-only origin main
 
 echo "[2/5] Parando containers existentes..."
-docker compose down
+docker-compose down
 
 echo "[3/5] Construindo imagens..."
-docker compose build --pull
+docker-compose build --pull
 
 echo "[4/5] Subindo containers..."
-docker compose up -d
+docker-compose up -d
 
 echo "[5/5] Executando migrations..."
-docker compose exec app php artisan migrate --force
+docker-compose exec app php artisan migrate --force
 
 echo "Deploy concluído com sucesso!"

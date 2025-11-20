4f8f025884239aefcfe335f2d6c89836ae317729
Wesley Silva <wesleydenia@gmail.com>
2025-11-20 19:25:39 +0000

    muda de call, para job o sync

--- routes/console.php
+++ routes/console.php
@@ -1,26 +1,12 @@
 <?php
 
 use App\Jobs\SyncVendusCouponsJob;
-use Illuminate\Support\Facades\Log;
 use App\Jobs\SyncAllUsersLoyaltyJob;
-use Illuminate\Foundation\Inspiring;
 use App\Jobs\SyncPendingCustomersJob;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::job(new SyncPendingCustomersJob())->everyFiveMinutes()->withoutOverlapping();
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::job(new SyncVendusCouponsJob())->everyFiveMinutes()->withoutOverlapping();
 
-Schedule::job(new SyncPendingCustomersJob())
-    ->everyTenMinutes()
-    ->name('sync-pending-customers')
-    ->withoutOverlapping();
-
-Schedule::job(new SyncVendusCouponsJob())->everyTenMinutes();
-
-Schedule::job((new \App\Jobs\SyncAllUsersLoyaltyJob(20))->onQueue('sync-loyalty'))
-    ->name('sync-loyalty')
-    ->everyTenMinutes()
-    ->withoutOverlapping();
+Schedule::job((new SyncAllUsersLoyaltyJob(20)))->everyFiveMinutes()->withoutOverlapping();

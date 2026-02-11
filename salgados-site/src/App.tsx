94cf9a090d0df428ec941c07101764deb922388f
Wesley Silva <wesleydenia@gmail.com>
2026-02-11 17:21:15 +0000

    Melhoria no SEO

--- salgados-site/src/App.tsx
+++ salgados-site/src/App.tsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { SiteLayout } from "@/components/SiteLayout";
-import Index from "./pages/Index";
-import Festas from "./pages/Festas";
-import Produtos from "./pages/Produtos";
-import Sobre from "./pages/Sobre";
-import Contactos from "./pages/Contactos";
-import Termos from "./pages/Termos";
-import Privacidade from "./pages/Privacidade";
-import NotFound from "./pages/NotFound";
+
+const Index = lazy(() => import("./pages/Index"));
+const Festas = lazy(() => import("./pages/Festas"));
+const Produtos = lazy(() => import("./pages/Produtos"));
+const Sobre = lazy(() => import("./pages/Sobre"));
+const Contactos = lazy(() => import("./pages/Contactos"));
+const Termos = lazy(() => import("./pages/Termos"));
+const Privacidade = lazy(() => import("./pages/Privacidade"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const queryClient = new QueryClient();
 
@@ -33,19 +34,21 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
-        <Routes>
-          <Route element={<SiteLayout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/festas" element={<Festas />} />
-            <Route path="/produtos" element={<Produtos />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/contactos" element={<Contactos />} />
-            <Route path="/termos" element={<Termos />} />
-            <Route path="/privacidade" element={<Privacidade />} />
-          </Route>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route element={<SiteLayout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/festas" element={<Festas />} />
+              <Route path="/produtos" element={<Produtos />} />
+              <Route path="/sobre" element={<Sobre />} />
+              <Route path="/contactos" element={<Contactos />} />
+              <Route path="/termos" element={<Termos />} />
+              <Route path="/privacidade" element={<Privacidade />} />
+            </Route>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>

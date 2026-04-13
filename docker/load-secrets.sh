1aafc21cf287da5ae10b81174d000935092ccfc6
Wesley Silva <wesleydenia@gmail.com>
2026-04-13 17:04:43 +0100

    implementa google API Key

--- docker/load-secrets.sh
+++ docker/load-secrets.sh
@@ -6,6 +6,9 @@ set -e
 [ -f "$MAIL_PASSWORD_FILE" ] && export MAIL_PASSWORD=$(cat "$MAIL_PASSWORD_FILE")
 [ -f "$VENDUS_API_KEY_FILE" ] && export VENDUS_API_KEY=$(cat "$VENDUS_API_KEY_FILE")
 [ -f "$WAPIFY_API_KEY_FILE" ] && export WAPIFY_API_KEY=$(cat "$WAPIFY_API_KEY_FILE")
+[ -f "$GOOGLE_API_KEY_FILE" ] && export GOOGLE_API_KEY=$(cat "$GOOGLE_API_KEY_FILE")
 
 echo "✅ Secrets carregados com sucesso. Iniciando PHP-FPM..."
 exec "$@"
+
+
